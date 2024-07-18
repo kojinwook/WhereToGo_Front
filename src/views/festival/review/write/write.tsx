4faf7eff8852edccfe69126d5bb8381b4fd1222e
KYJ1108 <108640336+KYJ1108@users.noreply.github.com>
2024-07-18 16:44:06 +0900

    모임 리스트 카테고리 디자인 (#63)
    * 비밀번호찾기 버튼 수정/ 문의, 문의접수, 리스트 화면 디자인
    
    * 문의 내용 수정 화면 디자인 수정
    
    * pull inquire
    
    * meeting write pull
    
    * 모임 개설, 디테일 디자인
    
    * pull
    
    * 모임 리스트 개별 리스트 비율 수정
    
    * 모임 리스트 카테고리 디자인

--- src/views/festival/review/write/write.tsx
+++ src/views/festival/review/write/write.tsx
@@ -86,7 +86,7 @@ export default function ReviewWritePage() {
     };
 
     return (
-        <div className='container'>
+        <div className='review-write-container'>
             <p><strong>별점</strong></p>
             <label>
                 {[...Array(5)].map((_, index) => (
@@ -102,7 +102,7 @@ export default function ReviewWritePage() {
             </label>
             <hr />
             <p><strong>내용</strong></p>
-            <label className='content'>
+            <label className='review-write-content'>
                 <textarea value={review} onChange={handleReviewChange} />
             </label>
             <hr />
@@ -112,11 +112,13 @@ export default function ReviewWritePage() {
                 {imagePreviews.map((preview, index) => (
                     <div key={index} style={{ position: 'relative', marginRight: '10px', marginBottom: '10px' }}>
                         <img
+                        className='review-write-img'
                             src={preview}
                             alt={`이미지 미리보기 ${index}`}
                             style={{ width: '100px', height: 'auto', marginRight: '10px' }}
                         />
                         <button
+                            className='review-write-btn'
                             style={{ position: 'absolute', top: '5px', right: '5px', background: 'none', border: 'none', cursor: 'pointer' }}
                             onClick={() => handleImageRemove(index)}
                         >
@@ -126,7 +128,7 @@ export default function ReviewWritePage() {
                 ))}
             </div>
             <hr />
-            <button onClick={handleSubmit}>리뷰 등록</button>
+            <button className='review-write-btn' onClick={handleSubmit}>리뷰 등록</button>
         </div>
     );
 };
