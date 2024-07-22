2521b501b53c8914156376026fcc263473914119
orderabbit <dur369667@naver.com>
2024-07-22 10:22:28 +0900

    마이페이지 모임리스트

---
+++ src/apis/response/meeting/board/get-user-board-list.response.dto.ts
@@ -0,0 +1,5 @@
+import ResponseDto from "apis/response/response.dto";
+import MeetingBoard from "types/interface/meeting-board.interface";
+
+export default interface GetUserBoardListResponseDto extends ResponseDto{
+}
