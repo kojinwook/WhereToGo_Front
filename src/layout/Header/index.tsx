8f1764fb5c8d352eebfbabc100228415dc0a492e
orderabbit <dur369667@naver.com>
2024-08-02 16:55:10 +0900

    a

--- src/layout/Header/index.tsx
+++ src/layout/Header/index.tsx
@@ -23,7 +23,7 @@ export default function Header() {
 
   useEffect(() => {
     if (loginUser && cookies.accessToken) {
-      const socket = new SockJS('http://localhost:8080/ws');
+      const socket = new SockJS('http://15.165.24.165:8088/ws');
       const stompClient = new Client({
         webSocketFactory: () => socket,
         // debug: (str) => console.log(str),
