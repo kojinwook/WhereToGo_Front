dc2cbdf7f36bf330c68aa794cbd940d0c0420b27
orderabbit <dur369667@naver.com>
2024-08-09 14:34:12 +0900

    a

--- src/views/user/modifyProfile/modifyProfile.tsx
+++ src/views/user/modifyProfile/modifyProfile.tsx
@@ -150,7 +150,7 @@ export default function UserModifyProfile() {
     }, [userId]);
 
     const handleBackSubmit = () => {
-        navigate ('/user/profile')
+        navigate (`/user/profile?userId=${userId}`);
     }
 
     return (
